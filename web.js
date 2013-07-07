e805f4e7593c598c4f43793076cc8ee1dd258388
courseraiopi <coursera.iopi@gmail.com>
2013-07-07 09:57:13 +0000

    Update to web.js to load index.html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var http = require('http');
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.write(fs.readFileSync("./index.html", "utf8").toString());
+   response.end();
 });
 
 var port = process.env.PORT || 5000;
